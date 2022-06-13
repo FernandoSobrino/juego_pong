d7c167445fb6178dfa22fe83759925e9e7bc3cc3
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-06-13 22:46:51 +0200

    feat: paleta ya se puede mover

--- pong.py
+++ pong.py
@@ -1,8 +1,25 @@
 import pygame
 
+ALTO_PALETA = 80
+ANCHO_PALETA = 5
+
 
 class Paleta(pygame.Rect):
-    pass
+    ARRIBA = True
+    ABAJO = False
+
+    def __init__(self,x,y):
+        super(Paleta,self).__init__(x,y, ANCHO_PALETA,ALTO_PALETA)
+        self.velocidad = 3
+
+    def mover_paleta(self,direccion):
+        if direccion == self.ARRIBA:
+            self.y = self.y - self.velocidad
+        else:
+            self.y = self.y + self.velocidad
+        pass
+
+
 
 class Linea(pygame.Rect):
     pass
@@ -12,11 +29,9 @@ class Pong:
     _ANCHO = 640
     _ALTO = 480
     _MARGEN_LATERAL = 30
-    _ANCHO_PALETA = 5
     _ANCHO_RED = 5
-    _ALTO_PALETA = _ALTO / 5
     _COLOR_PANTALLA = (0,128,0)
-    _BLANCO = (255,255,255)
+    _COLOR_BLANCO = (255,255,255)
 
     def __init__(self):
         print("Construyendo un objeto pong")
@@ -25,15 +40,13 @@ class Pong:
         self.pantalla.fill(self._COLOR_PANTALLA)
         self.jugador1 = Paleta(
             self._MARGEN_LATERAL,               #COORDENADA X (LEFT)
-            (self._ALTO-self._ALTO_PALETA)/2,   #COORDENADA Y (TOP)
-            self._ANCHO_PALETA,                 #ANCHO (WIDTH)
-            self._ALTO_PALETA)                  #ALTO (HEIGHT)              
+            (self._ALTO-ALTO_PALETA)/2)         #COORDENADA Y (TOP)
+                                      
         
         self.jugador2 = Paleta(
-            self._ANCHO - self._MARGEN_LATERAL - self._ANCHO_PALETA,
-            (self._ALTO-self._ALTO_PALETA)/2,
-            self._ANCHO_PALETA, 
-            self._ALTO_PALETA)
+            self._ANCHO - self._MARGEN_LATERAL - ANCHO_PALETA,
+            (self._ALTO-ALTO_PALETA)/2)
+           
 
         self.linea = Linea(self._ANCHO/2,0,self._ANCHO_RED,self._ALTO)
 
@@ -45,24 +58,44 @@ class Pong:
         while not salir_juego:
             eventos = pygame.event.get()
             for evento in eventos:
+                estado_teclas = pygame.key.get_pressed()
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == pygame.K_ESCAPE:
                         salir_juego = True
                 if evento.type == pygame.QUIT:
                     salir_juego = True
+                
+            if estado_teclas[pygame.K_a]:
+                self.jugador1.mover_paleta(Paleta.ARRIBA)
+            if estado_teclas[pygame.K_z]:
+                self.jugador1.mover_paleta(Paleta.ABAJO)
+            if estado_teclas[pygame.K_UP]:
+                self.jugador2.mover_paleta(Paleta.ARRIBA)
+            if estado_teclas[pygame.K_DOWN]:
+                    self.jugador2.mover_paleta(Paleta.ABAJO)
+                
+            self.pantalla.fill(self._COLOR_PANTALLA)
+                   
             
-            pygame.draw.rect(self.pantalla,self._BLANCO,self.jugador1)
-            pygame.draw.rect(self.pantalla,self._BLANCO,self.jugador2)
-            pygame.draw.rect(self.pantalla,self._BLANCO,self.linea)
+            pygame.draw.rect(self.pantalla,self._COLOR_BLANCO,self.jugador1)
+            pygame.draw.rect(self.pantalla,self._COLOR_BLANCO,self.jugador2)
+            #pygame.draw.rect(self.pantalla,self._BLANCO,self.linea)
+
+            for posicion in range(0, self._ALTO,45):
+                pygame.draw.line(self.pantalla,self._COLOR_BLANCO,
+                                (self._ANCHO/2, posicion),
+                                (self._ANCHO/2,posicion + 25))
+                
 
             #Dibujo del campo de tenis (molaría llevarlo a una función)
-            pygame.draw.rect(self.pantalla,self._BLANCO,(70,45,500,390),2)
-            pygame.draw.line(self.pantalla,self._BLANCO,(70,100),(567,100))
-            pygame.draw.line(self.pantalla,self._BLANCO,(70,380),(567,380))
-            pygame.draw.line(self.pantalla,self._BLANCO,(175,100),(175,380))
-            pygame.draw.line(self.pantalla,self._BLANCO,(470,100),(470,380))
-            pygame.draw.line(self.pantalla,self._BLANCO,(175,240),(470,240))
-        
+            
+            pygame.draw.rect(self.pantalla,self._COLOR_BLANCO,(70,45,500,390),2)
+            pygame.draw.line(self.pantalla,self._COLOR_BLANCO,(70,100),(567,100))
+            pygame.draw.line(self.pantalla,self._COLOR_BLANCO,(70,380),(567,380))
+            pygame.draw.line(self.pantalla,self._COLOR_BLANCO,(175,100),(175,380))
+            pygame.draw.line(self.pantalla,self._COLOR_BLANCO,(470,100),(470,380))
+            pygame.draw.line(self.pantalla,self._COLOR_BLANCO,(175,240),(470,240))
+            
 
             pygame.display.flip()
             
