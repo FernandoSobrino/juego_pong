fd7aae5f32218f01c1d1f4642e73fd6fe2e053c7
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-06-13 20:38:08 +0200

    feat: agregamos fin juego con escape

--- pong.py
+++ pong.py
@@ -1,26 +1,28 @@
 import pygame
 
+
 class Paleta(pygame.Rect):
+    pass
+
+class Linea(pygame.Rect):
     pass
 
 class Pong:
 
     _ANCHO = 640
     _ALTO = 480
-    _MARGEN_LATERAL = 40
+    _MARGEN_LATERAL = 30
     _ANCHO_PALETA = 5
+    _ANCHO_RED = 5
     _ALTO_PALETA = _ALTO / 5
-    _COLOR_PANTALLA = (0,210,0)
-
-
-
-
-
+    _COLOR_PANTALLA = (0,128,0)
+    _BLANCO = (255,255,255)
 
     def __init__(self):
         print("Construyendo un objeto pong")
         pygame.init()
         self.pantalla = pygame.display.set_mode((self._ANCHO,self._ALTO))
+        self.pantalla.fill(self._COLOR_PANTALLA)
         self.jugador1 = Paleta(
             self._MARGEN_LATERAL,               #COORDENADA X (LEFT)
             (self._ALTO-self._ALTO_PALETA)/2,   #COORDENADA Y (TOP)
@@ -33,18 +35,35 @@ class Pong:
             self._ANCHO_PALETA, 
             self._ALTO_PALETA)
 
+        self.linea = Linea(self._ANCHO/2,0,self._ANCHO_RED,self._ALTO)
+
+    
+
     def bucle_principal(self):
         print("Estoy en el bucle principal")
         salir_juego = False
         while not salir_juego:
             eventos = pygame.event.get()
             for evento in eventos:
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_ESCAPE:
+                        salir_juego = True
                 if evento.type == pygame.QUIT:
                     salir_juego = True
-            self.pantalla.fill(self._COLOR_PANTALLA)
-            pygame.draw.rect(self.pantalla,(255,255,255),self.jugador1)
-            pygame.draw.rect(self.pantalla,(255,255,255),self.jugador2)
-            pygame.draw.line(self.pantalla,(255,255,255),(self._ANCHO/2,self._ALTO),(self._ANCHO/2,0))
+            
+            pygame.draw.rect(self.pantalla,self._BLANCO,self.jugador1)
+            pygame.draw.rect(self.pantalla,self._BLANCO,self.jugador2)
+            pygame.draw.rect(self.pantalla,self._BLANCO,self.linea)
+
+            #Dibujo del campo de tenis (molaría llevarlo a una función)
+            pygame.draw.rect(self.pantalla,self._BLANCO,(70,45,500,390),2)
+            pygame.draw.line(self.pantalla,self._BLANCO,(70,100),(567,100))
+            pygame.draw.line(self.pantalla,self._BLANCO,(70,380),(567,380))
+            pygame.draw.line(self.pantalla,self._BLANCO,(175,100),(175,380))
+            pygame.draw.line(self.pantalla,self._BLANCO,(470,100),(470,380))
+            pygame.draw.line(self.pantalla,self._BLANCO,(175,240),(470,240))
+        
+
             pygame.display.flip()
             
             
